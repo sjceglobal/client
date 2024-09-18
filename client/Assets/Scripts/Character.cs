a34439a4b662ec60e0327df40c9602c134851295
limsg <limsg0510@gmail.com>
2024-09-18 14:20:25 +0900

    캐릭터, 몬스터 수정

--- client/Assets/Scripts/Character.cs
+++ client/Assets/Scripts/Character.cs
@@ -15,23 +15,27 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _speed = 10f;
+        _speed = 100f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //float x = this.transform.localPosition.x + (Time.deltaTime * _speed);
+		//this.transform.localPosition = new Vector3(x, 22);
         this.transform.Translate(new Vector3(Time.deltaTime *_speed,0));  
-		if(_speed == 0f)
-			_anim.SetInteger ("playerState",(int)playerState.attack1);	
+		// if(_speed == 0f)
+		// 	_anim.SetInteger ("playerState",(int)playerState.attack1);	
     }
 
     void OnTriggerEnter(Collider other){
-        Debug.Log(other);
+        Debug.Log(other.transform);
+        //Debug.Log(other == "monster");
 		if (other.transform.tag == "enemy") {
 			Debug.Log("====Character Trigger start====");
 			_speed = 0f;
-			_anim.SetInteger ("playerState",(int)playerState.attack1);
+			//_anim.SetInteger ("playerState",(int)playerState.attack1);
+            _anim.Play("attack");
 		}
 	}
 }
